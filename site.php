dd08d57e9224d915bc4685077d3a00c405c0d14a
lionjasmin62 <138154924+lionjasmin62@users.noreply.github.com>
2026-07-16 08:51:04 +0200

    Integrate Statcounter for web analytics
    Added Statcounter tracking code to the site.

--- site.php
+++ site.php
@@ -265,5 +265,23 @@
       loadSecret();
     }, { once: true });
   </script>
+
+  <!-- Default Statcounter code for desk http://pornserde.com
+-->
+<script type="text/javascript">
+var sc_project=13301707; 
+var sc_invisible=1; 
+var sc_security="8c11122f"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/13301707/0/8c11122f/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
 </body>
 </html>
